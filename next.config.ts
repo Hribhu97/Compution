09f4ac87fec6c668d7bd609eb9965cc328e49387
Hribhu Tapadar <tapadarhribhu@gmail.com>
2026-08-19 16:02:29 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
